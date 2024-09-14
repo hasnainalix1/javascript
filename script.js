1ff5969b4e34e174fccce93ebfad1c31e8556ae6
hasnian1122 <soomronain22@gmail.com>
2024-09-14 10:54:44 -0700

    javascript

---
+++ script.js
@@ -0,0 +1,35 @@
+
+//alert
+// alert("accept cookies");    
+
+
+
+//name, age
+var a = ("Hasnain Ali  ");
+var b = ( 19);
+var c = (a + b);
+console.log (c);
+
+//addition
+var q = (17);
+var r = (48);
+var s = (q + r);
+console.log ("17 + 48 = " + s);
+
+//subscription
+var q = (117);
+var r = (58);
+var s = (q - r);
+console.log ("117 - 58 = " + s);
+
+//multiplication
+var q = (197);
+var r = (18);
+var s = (q * r);
+console.log ("17  x  48 = " + s);
+
+//Illegal Variable
+// var function = (12);
+// var my-var = (10);
+// var answer =(function + my-var);
+// console.log (answer);
