a103acb2f371e41092388febf7b5f12b29d9c1a5
zaid <zaid69232@gmail.com>
2024-09-20 15:03:18 +0500

    compelete

--- five.js
+++ five.js
@@ -1,10 +1,51 @@
-// one
-alert ("Error! Please enter a valid password.")
-// two
-alert ("Welcome to JS land..\nHappy coding!")
-// three
-alert ("Happy Coding!\nPrevent this page from creating additional dialogs.")
-// four
-alert ("Hello\nI can rus JS through my web browser's console")
-// five
-alert("You have visited this site 4 times")
+// sum of number
+var one = 3;
+var two = 5;
+let total = one + two;
+var three = "Sum of 3 and 5 is " + total + "<br>";
+document.write(three);
+
+// cost of tickets
+let costOfTicket = 600;
+let numberOfTickets = 5;
+let totalCost = costOfTicket * numberOfTickets;
+let print = "Total cost to buy " + numberOfTickets + " tickets to a movie is " + totalCost + "<br>";
+document.write(print);
+
+// table of 4
+var number = 4;
+var result1 = number * 1;
+var result2 = number * 2;
+var result3 = number * 3;
+var result4 = number * 4;
+var result5 = number * 5;
+var result6 = number * 6;
+var result7 = number * 7;
+var result8 = number * 8;
+var result9 = number * 9;
+var result10 = number * 10;
+
+document.write("Multiplication Table of " + number + "<br><br>");
+document.write(number + " x 1 = " + result1 + "<br>");
+document.write(number + " x 2 = " + result2 + "<br>");
+document.write(number + " x 3 = " + result3 + "<br>");
+document.write(number + " x 4 = " + result4 + "<br>");
+document.write(number + " x 5 = " + result5 + "<br>");
+document.write(number + " x 6 = " + result6 + "<br>");
+document.write(number + " x 7 = " + result7 + "<br>");
+document.write(number + " x 8 = " + result8 + "<br>");
+document.write(number + " x 9 = " + result9 + "<br>");
+document.write(number + " x 10 = " + result10 + "<br>");
+
+// mark sheet
+var totalmark = 980;
+var obtained = 804;
+var percentage = (totalmark / obtained * 100);
+document.write (percentage + "%" +"<br>");
+
+// 
+var one = 104;
+var two = 5;
+let tota = one + two;
+var three = "Sum of 3 and 5 is " + total + "<br>";
+document.write(three);
